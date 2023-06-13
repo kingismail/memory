21b5b1f68f37ad8840bd828fa31e4df3742902c1
Ismail <68245583+kingismail@users.noreply.github.com>
2023-06-13 10:34:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let images = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 1, 2, 3, 4, 5, 6 , 7, 8, 9];
 
+// game variables
+
+load_prompt_mobile = document.getElementById('loading_mobile');
+play_button_mobile = document.getElementById('play_button_mobile');
+
+let game_not_started = true;
+let on_mobile_phone = false;
+
+
 for (i = 1; i < 19; i++)
 {
     let image_face = document.getElementById(`image${i}`);
@@ -29,6 +38,20 @@ function turnCards()
         curr_element.classList.add('rotate_card');}
     }
 }
+
+function rotate_logo_card(number)
+{
+    logo_card = document.getElementById(`image${number}_logo`);
+    logo_card.parentElement.classList.add('rotate_card');
+}
+
+function return_logo_card_state(number)
+{
+    logo_card = document.getElementById(`image${number}_logo`);
+    logo_card.parentElement.classList.remove('rotate_card');
+}
+
+play_button_mobile.style.display = 'none';
 
 function returnCards()
 {
@@ -61,7 +84,6 @@ document.addEventListener('click', function(event){
     }
     if(open_cards.length === 2)
     {
-
         id_image1 = window.getComputedStyle(document.getElementById(open_cards[0])).getPropertyValue('background-image');
         id_image2 = window.getComputedStyle(document.getElementById(open_cards[1])).getPropertyValue('background-image');
 
@@ -72,8 +94,27 @@ document.addEventListener('click', function(event){
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    turnCards();
-    setTimeout(returnCards, 3000);
+    //turnCards();
+    //setTimeout(returnCards, 3000);
+
+    load_prompt_mobile = document.getElementById('loading_mobile');
+    play_button_mobile = document.getElementById('play_button_mobile');
+
+    for (let i = 1; i < 5; i++) {
+        (function(index) {
+          setTimeout(() => {
+            return_logo_card_state(index);
+            rotate_logo_card(index);
+            if(index > 1 && index < 5) return_logo_card_state(index - 1);
+            if (index === 4)
+            { setTimeout(() => return_logo_card_state(4), 1500);
+                load_prompt_mobile.style.display = 'none';
+                play_button_mobile.style.display = '';
+                play_button_mobile.classList.add('move_button');
+            }
+          }, index * 1500);
+        })(i);
+      }
   });
 
 for (i = 1; i < 1; i++)
@@ -81,4 +122,14 @@ for (i = 1; i < 1; i++)
     let image_face = document.getElementById(`image${i}`);
     console.log(image_face.parentElement.parentElement);
 }
+
+//Code for the mobile phones
+
+if (window.screen.width < 768) on_mobile_phone = true;
+
+
+
+
+
+
 
